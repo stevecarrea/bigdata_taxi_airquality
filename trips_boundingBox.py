0e02861e6a8a3e5f633de806b1bd75315bd9a6d7
bmpasini <bmpasini@gmail.com>
2015-04-22 00:34:01 -0400

    plot daily average

--- trips_boundingBox.py
+++ trips_boundingBox.py
@@ -12,13 +12,14 @@ count = 0
 speeds = {}
 start_time = datetime.now()
 measurements = {}
-with open('/Users/Steve/GDrive/NYU_CUSP/Big_Data/project/nysdec_queenscollege_jan2013_NOx.csv', 'r') as csvfile_air:
+with open('/Users/brunomacedo/Desktop/NYU-Poly/3rd-Semester/Big-Data/project/nysdec_queenscollege_jan2013_NOx.csv', 'r') as csvfile_air:
     reader = csv.reader(csvfile_air)
     next(reader)
     for row in reader:
         try:
-            date_time = row[0]+' '+row[1]
-            measurement_date = datetime.strptime(date_time, '%Y-%m-%d %H:%M')  # 2013-01-27
+            # date_time = row[0]+' '+row[1]
+            date_time = row[0]
+            measurement_date = datetime.strptime(date_time, '%Y-%m-%d')  # 2013-01-27
             monitor_measurement = float(row[4])  # NOx
             if measurement_date not in measurements.keys():
                 measurements[measurement_date] = []
@@ -27,21 +28,23 @@ with open('/Users/Steve/GDrive/NYU_CUSP/Big_Data/project/nysdec_queenscollege_ja
             pass
 
     for date in sorted(measurements):
+        measurement = math.ceil(reduce(lambda x, y: x + y, measurements[date]) / len(measurements[date]) * 100) / 100
         x_air.append(date)
-        y_air.append(measurements[date])
+        y_air.append(measurement)
+        print 'Date: ', date, ' | Average Measurement: ', round(measurement, 2), '| Number of Measurements: ', len(measurements[date])
 
-with open('/Users/Steve/GDrive/NYU_CUSP/Big_Data/project/trip_data_1.csv', 'r') as csvfile:
+with open('/Users/brunomacedo/Desktop/NYU-Poly/3rd-Semester/Big-Data/project/trip_data_1.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
     next(reader)
     for row in reader:
         # if count >= 5:  # for testing
         #     break
         try:
-            count += 1
+            # count += 1
             pickup_lon = float(row[10])
             pickup_lat = float(row[11])
-            pickup_date = datetime.strptime(row[5], '%Y-%m-%d %H:%M:%S')  # 2013-01-27 11:45:00
-            dropoff_date = datetime.strptime(row[6], '%Y-%m-%d %H:%M:%S')
+            pickup_date = datetime.strptime(row[5][:-9], '%Y-%m-%d')  # 2013-01-27 11:45:00
+            dropoff_date = datetime.strptime(row[6][:-9], '%Y-%m-%d')
             dropoff_lon = float(row[12])
             dropoff_lat = float(row[13])
             trip_distance = float(row[9])
@@ -62,15 +65,24 @@ with open('/Users/Steve/GDrive/NYU_CUSP/Big_Data/project/trip_data_1.csv', 'r') 
                     if trip_distance < 2.0:
                         speed = ( trip_distance / trip_duration ) * 3600
                         if speed > 0 and speed < 100:
+                            if pickup_date not in speeds.keys():
+                                speeds[pickup_date] = []
+                            speeds[pickup_date].append(speed)
 
-                            x.append(pickup_date)
-                            y.append(speed)
+                            # x.append(pickup_date)
+                            # y.append(speed)
 
                             print 'Date: ', pickup_date, 'Speed: ', round(speed, 2)
                             count += 1
         except:
             pass
 print 'Count: ', count
+
+for date in sorted(speeds):
+    speed = math.ceil(reduce(lambda x, y: x + y, speeds[date]) / len(speeds[date]) * 100) / 100
+    x.append(date)
+    y.append(speed)
+    print 'Date: ', date, '| Average Speed: ', speed, '| Number of Taxis: ', len(speeds[date])
 
 c = datetime.now() - start_time
 print 'It took', divmod(c.days * 86400 + c.seconds, 60), '(minutes, seconds).'
