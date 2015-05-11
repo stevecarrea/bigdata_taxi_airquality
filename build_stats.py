30b58e923becffb638d9f5a676aa1946204d16ee
alldayiwonder <carrea.steve@gmail.com>
2015-05-10 23:22:38 -0400

    Updates build_stats w final output
    this may be working now

--- build_stats.py
+++ build_stats.py
@@ -1,7 +1,10 @@
+import csv
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.pylab as pylab
 from datetime import datetime
+from scipy.stats import linregress
+import numpy as np
 
 # Parse dates into datetime objects
 def parse_date(raw_date):
@@ -22,40 +25,37 @@ def plot_avgSpeed():
         plt.show()
 
 # Get slope and r-squared value for each day of the best fit line b/w avg speed and pollutant
+output = []
 def daily_stats(df):  
+    # CCNY 
+    df_ccny = df[df['Location'] == 'CCNY']
+    for i, group in df_ccny.groupby(df_ccny['Time'].dt.date):
+        monitor = 'CCNY'
+        pollutant = 'PM25_Acceptable'
 
-    df_CCNY = df[(df.Location == 'CCNY')]  # Subset dataframe for specific monitor location
-    for pollutant in air_monitors['CCNY']:
-        for i in xrange(0, len(air_monitors['CCNY'])):     
-            df.groupby(df['Time'].dt.day).plot(x='Average_Speed', y=air_monitors['CCNY'][i], style = 'o', title='CCNY'+": "+air_monitors['CCNY'][i]+" "+str(i))
-            plt.savefig('plots/plot'+str('CCNY_')+str(air_monitors['CCNY'][i])+'_'+str(i)+'.png') 
+        group.plot(x='Average_Speed', y=pollutant, style = 'o', title=monitor+": "+pollutant+" "+str(i))
+        #plt.savefig('plots/plot'+str(monitor)+str(pollutant)+'_'+str(i)+'.png') 
+        
+        # fit with scipy
+        slope, intercept, r, p, stderr = linregress(group['Average_Speed'], group[pollutant])
+        print i, slope, r
+        output.append((i, slope, r))
 
-    df_PS19 = df[(df.Location == 'PS19')]
-    for pollutant in air_monitors['PS19']:
-        for i in xrange(0, len(air_monitors['PS19'])):     
-            df.groupby(df['Time'].dt.day).plot(x='Average_Speed', y=air_monitors['PS19'][i], style = 'o', title='PS19'+": "+air_monitors['PS19'][i]+" "+str(i))
-            plt.savefig('plots/plot'+str('PS19_')+str(air_monitors['PS19'][i])+'_'+str(i)+'.png') 
-
-    df_DivisionStreet = df[(df.Location == 'DivisionStreet')]
-    for pollutant in air_monitors['DivisionStreet']:
-        for i in xrange(0, len(air_monitors['DivisionStreet'])):     
-            df.groupby(df['Time'].dt.day).plot(x='Average_Speed', y=air_monitors['DivisionStreet'][i], style = 'o', title='DivisionStreet'+": "+air_monitors['DivisionStreet'][i]+" "+str(i))
-            plt.savefig('plots/plot'+str('DivisionStreet_')+str(air_monitors['DivisionStreet'][i])+'_'+str(i)+'.png') 
-
+    with open('output_final.csv', "wb") as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
+            for line in output:
+                writer.writerow(line)
 
 # Plot number of taxis per hour for each monitor for entire time period to see density over time
 def plot_density(x):
     pass
 
-
-
 air_monitors = { 'CCNY': ('PM25_Acceptable', 'PM25_Raw', 'CO', 'Ozone'),
                  'PS19': ('PM25_Acceptable', 'PM25_Raw'),
                  'DivisionStreet': ('PM25_Acceptable', 'PM25_Raw') }
 
-# Headers as: Location,Time,Number_Taxis,Average_Speed,PM25_Acceptable,Ozone,CO,PM25_Raw,Sky_Condition,Relative_Humidity,Wind_Speed,Wind_Direction
-
 # Read file output from MapReduce, comma-separated
+# Headers as: Location,Time,Number_Taxis,Average_Speed,PM25_Acceptable,Ozone,CO,PM25_Raw,Sky_Condition,Relative_Humidity,Wind_Speed,Wind_Direction
 df = pd.read_csv('part-00000', delimiter=',')
 
 # Convert raw dates to datetime objects
@@ -64,7 +64,10 @@ df['Time'] = df['Time'].map(parse_date)
 #print df
 #print_full(df)
 #plot_avgSpeed()
+#daily_stats(df)
+
 daily_stats(df)
+
 
 
 
